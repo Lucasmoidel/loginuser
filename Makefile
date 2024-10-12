f91f71445a1304017cc61a719bd04cf2bcb3046a
Lucasmoidel <99782884+Lucasmoidel@users.noreply.github.com>
2024-10-12 10:34:19 -0700

    stuff

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ build:
 	g++ main.cpp -o main -Wall -g
 run:
 	./main
+win:
+	x86_64-w64-mingw32-g++ main.cpp -o main.exe -Wall
+clean:
+	rm main passdb.txt && touch passdb.txt && rm main.exe
--- main.cpp
+++ main.cpp
@@ -12,13 +12,13 @@ void signUp();
 void login();
 
 int main(int argc, char *argv[]) {
-    string filetext;
     string inpusername;
     hash<string> hasher;
     string inppass;
     
     if (argc == 2) {
         if (!strcmp(argv[1],"login")) {
+            string filetext;
             cout << "Username: ";
             cin >> inpusername;
             cout << "Password: ";
@@ -34,15 +34,27 @@ int main(int argc, char *argv[]) {
             }
             cout << "login failed \n";
         } else if (!strcmp(argv[1],"signup")) {
+            string filetext;
             cout << "Username: ";
             cin >> inpusername;
             cout << "Password: ";
             cin >> inppass;
+
             size_t hashpass = hasher(inppass);
-            ofstream fpass;
-            fpass.open("passdb.txt", ios::app);
-            fpass << inpusername << ": " << (hashpass) << "\n";
-            fpass.close();
+            
+            ifstream inpass("passdb.txt");
+            while (getline(inpass, filetext)){
+                size_t pos = filetext.find(':');
+                if (inpusername == filetext.substr(0, pos)) {
+                    cout << "there is already a user with that name.\n";
+                    inpass.close();
+                    return 0;
+                }
+            }
+            ofstream outpass;
+            outpass.open("passdb.txt", ios::app);
+            outpass << inpusername << ": " << (hashpass) << "\n";
+            outpass.close();
             return 0;
 
 
