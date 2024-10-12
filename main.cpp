a8b70dfb758d1b27ac6ecafbbc9974cad89f10b4
Lucasmoidel <99782884+Lucasmoidel@users.noreply.github.com>
2024-10-12 10:04:03 -0700

    works

--- main.cpp
+++ main.cpp
@@ -12,8 +12,6 @@ void signUp();
 void login();
 
 int main(int argc, char *argv[]) {
-    
-    int linecount = 0;
     string filetext;
     string inpusername;
     hash<string> hasher;
@@ -26,21 +24,15 @@ int main(int argc, char *argv[]) {
             cout << "Password: ";
             cin >> inppass;
             size_t hashpass = hasher(inppass);
-            //cout << hashpass;
-            ifstream fpass;
-            fpass.open("passdb.txt", ios::in);
+            ifstream fpass("passdb.txt");
             while (getline (fpass, filetext)) {
-                linecount++;
-            }
-            for (int i = 0; i < linecount; i++) {
-                getline(fpass, filetext);
-                //replace( filetext.begin(), filetext.end(), '\n', '\0');
-                if (filetext == to_string(hashpass)) {
+                if (filetext == (inpusername + ": " +to_string(hashpass))) {
                     cout << "login succesful\n";
                     fpass.close();
                     return 0;
                 }
             }
+            cout << "login failed \n";
         } else if (!strcmp(argv[1],"signup")) {
             cout << "Username: ";
             cin >> inpusername;
@@ -49,7 +41,7 @@ int main(int argc, char *argv[]) {
             size_t hashpass = hasher(inppass);
             ofstream fpass;
             fpass.open("passdb.txt", ios::app);
-            fpass << to_string(hashpass);
+            fpass << inpusername << ": " << (hashpass) << "\n";
             fpass.close();
             return 0;
 
